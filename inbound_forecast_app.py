6c624a9525ef6d71f5dfd55ef1d0e30a3081abce
hieungaann <139311086+hieungaann@users.noreply.github.com>
2025-09-24 17:32:44 +0700

    Update inbound_forecast_app.py

--- inbound_forecast_app.py
+++ inbound_forecast_app.py
@@ -29,8 +29,8 @@ label, .stMarkdown { color:#000000 !important; }
 
 div.stButton > button { background-color:#ff66b2; color:#000000; border-radius:12px; height:3em; width:100%; font-size:18px; transition:0.3s; }
 div.stButton > button:hover { background-color:#ff3385; transform: scale(1.05); }
-.stDownloadButton button { background-color:#ff99cc; color:#000000; border-radius:12px; height:3em; font-size:18px; transition:0.3s; }
-.stDownloadButton button:hover { background-color:#ff4da6; transform: scale(1.05); }
+.stDownloadButton button { background-color:#ff66b2; color:#000000; border-radius:12px; height:3em; font-size:18px; transition:0.3s; }
+.stDownloadButton button:hover { background-color:#ff3385; transform: scale(1.05); }
 .stAlert { border-radius:15px; padding:12px; font-size:16px; color:#000000; }
 
 [data-testid="stFileUploader"] button {
