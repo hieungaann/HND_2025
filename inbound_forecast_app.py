1ae519cf645ce69eb72e95d79f841289ef5546f5
hieungaann <139311086+hieungaann@users.noreply.github.com>
2025-09-24 17:04:22 +0700

    Update inbound_forecast_app.py

--- inbound_forecast_app.py
+++ inbound_forecast_app.py
@@ -37,7 +37,7 @@ div.stButton > button:hover { background-color:#ff3385; transform: scale(1.05); 
 
 /* Alert */
 .stAlert {
-  background:#111 !important;        /* đen */
+  background:#ff2f8f !important;        /* hồng */
   color:#fff !important;             /* chữ trắng */
   border-left:5px solid #ff5aa7 !important;
   border-radius:10px !important;
