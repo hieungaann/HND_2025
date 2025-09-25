6ee42debfe41e7bcfd59c136cd47e413ba077933
hieungaann <139311086+hieungaann@users.noreply.github.com>
2025-09-25 23:08:42 +0700

    Update inbound_forecast_app.py

--- inbound_forecast_app.py
+++ inbound_forecast_app.py
@@ -31,7 +31,7 @@ st.markdown(
     div.stButton > button:hover,
     .stDownloadButton button:hover,
     [data-testid="stFileUploader"] button:hover {
-        background:#ff66b2; border-color:#ff66b2; transform: scale(1.05);
+        background:#ff3385; border-color:#ff3385; transform: scale(1.05);
     }
 
     .stAlert {
